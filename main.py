27026208946e6fbf90385d68de0b9d43344922d0
Amitabh Kumar <amitabh.kumar2@gmail.com>
2021-11-28 21:17:24 -0500

    add

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+user_input = input("enter goal\n")
+input_list = user_input.split(":")
 
+goal = input_list[0]
+deadline = input_list[1]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%y")
 
+today_date = datetime.datetime.today()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(deadline_date - today_date)
