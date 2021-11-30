97101ab1b217c0897cf87cd9daf68ad11d553a8a
Amitabh Kumar <amitabh.kumar2@gmail.com>
2021-11-30 07:46:52 -0500

    add

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-import datetime
+# import datetime
+#
+# user_input = input("enter goal\n")
+# input_list = user_input.split(":")
+#
+# goal = input_list[0]
+# deadline = input_list[1]
+#
+# deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%y")
+#
+# today_date = datetime.datetime.today()
+#
+# time_till = deadline_date - today_date
+#
+# print(f"Dear User Time remaining for your goal : {goal} is {time_till.days} days")
+#
+import openpyxl
 
-user_input = input("enter goal\n")
-input_list = user_input.split(":")
 
-goal = input_list[0]
-deadline = input_list[1]
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
 
-deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%y")
+products_per_supplier = {}
 
-today_date = datetime.datetime.today()
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    print(supplier_name)
 
-print(deadline_date - today_date)
+    # calculation for number of products
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier[supplier_name]
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        products_per_supplier[supplier_name] = 1
+
+print(products_per_supplier)
